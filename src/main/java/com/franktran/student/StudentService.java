ddfb5d01c61dce5967ad6181f57805ed91b163fd
Frank Tran <dss_pm_se1@vtijs.com>
2021-04-04 10:29:26 +0700

    Update

--- src/main/java/com/franktran/student/StudentService.java
+++ src/main/java/com/franktran/student/StudentService.java
@@ -1,12 +1,9 @@
 package com.franktran.student;
 
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@Service
 public class StudentService {
 
   public List<Student> getStudents() {
